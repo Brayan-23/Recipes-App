bbb4fb29e3a26a1a7ddf011951a4b40944b93dbd
lucascoimbra <lucas.gabriel_coimbra@hotmail.com>
2022-08-22 12:50:46 -0300

    Menu Inferior feito

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Drinks({ history }) {
   return (
     <div>
       <Header history={ history } title="Drinks" />
       Drinks
+      <Footer />
 
     </div>
   );
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Foods({ history }) {
   return (
     <div>
       <Header history={ history } title="Foods" />
+      <Footer />
+
     </div>
   );
 }
