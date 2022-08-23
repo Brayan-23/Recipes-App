f998e2d81bbb32229a721b16f5ce8863bd97e80d
Maristela Santos <maristela_sp@hotmail.com>
2022-08-23 15:37:29 -0300

    req 57

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Recipes from '../components/Recipes';
+import ProfileComponent from '../components/ProfileComponent';
 
 function Profile({ history }) {
   return (
     <div>
       <Header history={ history } title="Profile" />
       <Recipes title="Profile" />
-      Profile
+      <ProfileComponent />
       <Footer />
-
     </div>
   );
 }
