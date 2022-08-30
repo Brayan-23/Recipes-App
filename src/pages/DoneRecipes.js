27df25c2bf0ed46a7b1c85d7a8604ca19f409b3b
Fabiana Araujo <fnaraujo85@gmail.com>
2022-08-30 15:06:13 -0300

    button filter

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -8,69 +8,95 @@ import shareIcon from '../images/shareIcon.svg';
 
 function DoneRecipes({ history }) {
   const [doneRecipes, setDoneRecipes] = useState([]);
+  const [filter, setFilter] = useState('');
+  const allDRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
 
   useEffect(() => {
-    const allDRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
     if (allDRecipes) setDoneRecipes(allDRecipes);
   }, []);
 
   return (
     <div>
       <Header history={ history } title="Done Recipes" />
-      DoneRecipes
-      <button type="button" data-testid="filter-by-food-btn">Foods </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+        onClick={ () => setFilter('food') }
+      >
+        Foods
+        {' '}
 
-      <button type="button" data-testid="filter-by-drink-btn">Drinks </button>
+      </button>
 
-      <button type="button" data-testid="filter-by-all-btn">All </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ () => setFilter('drink') }
+      >
+        Drinks
+        {' '}
 
-      {doneRecipes.length !== 0 && doneRecipes.map((recipe, i) => (
-        <div id="mainAoQuadraxion" key={ recipe.id }>
-          <Link to={ `/${recipe.type}s/${recipe.id}` }>
-            <h2 data-testid={ `${i}-horizontal-name` }>{recipe.name}</h2>
-            <img
-              data-testid={ `${i}-horizontal-image` }
-              src={ recipe.image }
-              alt={ recipe.name }
-            />
-          </Link>
-          <p data-testid={ `${i}-horizontal-done-date` }>
-            { recipe.doneDate }
-          </p>
-          <p data-testid={ `${i}-${recipe.tags[0]}-horizontal-tag` }>
-            { recipe.tags[0] }
-          </p>
-          <p data-testid={ `${i}-${recipe.tags[1]}-horizontal-tag` }>
-            {recipe.tags[1]}
-          </p>
-          <button
-            type="button"
-            onClick={ ({ target }) => {
-              target.innerHTML = 'Link copied!';
-              copy(`http://localhost:3000/${recipe.type}s/${recipe.id}`);
-            } }
-          >
-            <img
-              alt="Botão de Compartilhar"
-              src={ shareIcon }
-              data-testid={ `${i}-horizontal-share-btn` }
-            />
-          </button>
+      </button>
 
-          <button type="button">
-            <img
-              alt="Botão de Favorito"
-              src={ favoriteIcon }
-              data-testid={ `${i}-horizontal-favorite-btn` }
-            />
-          </button>
-          <p data-testid={ `${i}-horizontal-top-text` }>
-            {recipe.type === 'drink'
-              ? recipe.alcoholicOrNot
-              : `${recipe.nationality} - ${recipe.category}`}
-          </p>
-        </div>
-      ))}
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ () => setFilter('') }
+      >
+        All
+        {' '}
+
+      </button>
+
+      {doneRecipes.length !== 0
+      && doneRecipes.filter((e) => (!filter ? e : e.type === filter))
+        .map((recipe, i) => (
+          <div id="mainAoQuadraxion" key={ recipe.id }>
+            <Link to={ `/${recipe.type}s/${recipe.id}` }>
+              <h2 data-testid={ `${i}-horizontal-name` }>{recipe.name}</h2>
+              <img
+                data-testid={ `${i}-horizontal-image` }
+                src={ recipe.image }
+                alt={ recipe.name }
+              />
+            </Link>
+            <p data-testid={ `${i}-horizontal-done-date` }>
+              { recipe.doneDate }
+            </p>
+            <p data-testid={ `${i}-${recipe.tags[0]}-horizontal-tag` }>
+              { recipe.tags[0] }
+            </p>
+            <p data-testid={ `${i}-${recipe.tags[1]}-horizontal-tag` }>
+              {recipe.tags[1]}
+            </p>
+            <button
+              type="button"
+              onClick={ ({ target }) => {
+                target.innerHTML = 'Link copied!';
+                copy(`http://localhost:3000/${recipe.type}s/${recipe.id}`);
+              } }
+            >
+              <img
+                alt="Botão de Compartilhar"
+                src={ shareIcon }
+                data-testid={ `${i}-horizontal-share-btn` }
+              />
+            </button>
+
+            <button type="button">
+              <img
+                alt="Botão de Favorito"
+                src={ favoriteIcon }
+                data-testid={ `${i}-horizontal-favorite-btn` }
+              />
+            </button>
+            <p data-testid={ `${i}-horizontal-top-text` }>
+              {recipe.type === 'drink'
+                ? recipe.alcoholicOrNot
+                : `${recipe.nationality} - ${recipe.category}`}
+            </p>
+          </div>
+        ))}
     </div>
   );
 }
--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -7,6 +7,7 @@ import shareIcon from '../images/shareIcon.svg';
 
 function FavoriteRecipes() {
   const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+  const [filter, setFilter] = useState('');
   const previousFavoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
 
   useEffect(() => {
@@ -26,54 +27,80 @@ function FavoriteRecipes() {
   return (
     <div id="main">
       <Header title="Favorite Recipes" />
-      <button type="button" data-testid="filter-by-food-btn">Foods </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+        onClick={ () => setFilter('food') }
+      >
+        Foods
+        {' '}
 
-      <button type="button" data-testid="filter-by-drink-btn">Drinks </button>
+      </button>
 
-      <button type="button" data-testid="filter-by-all-btn">All </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ () => setFilter('drink') }
+      >
+        Drinks
+        {' '}
 
-      {favoriteRecipes.length !== 0 && favoriteRecipes.map((recipe, i) => (
-        <div id="mainAoQuadraxion" key={ recipe.id }>
-          <Link to={ `/${recipe.type}s/${recipe.id}` }>
-            <h2 data-testid={ `${i}-horizontal-name` }>{recipe.name}</h2>
-            <img
-              data-testid={ `${i}-horizontal-image` }
-              src={ recipe.image }
-              alt={ recipe.name }
-            />
-          </Link>
-          <button
-            type="button"
-            data-testid={ `${i}-sharebutton` }
-            onClick={ ({ target }) => {
-              target.innerHTML = 'Link copied!';
-              copy(`http://localhost:3000/${recipe.type}s/${recipe.id}`);
-            } }
-          >
-            <img
-              alt="Botão de Compartilhar"
-              src={ shareIcon }
-              data-testid={ `${i}-horizontal-share-btn` }
-            />
-          </button>
+      </button>
 
-          <button
-            type="button"
-            onClick={ () => removeFavoriteHandle(i) }
-          >
-            <img
-              alt="Botão de Favorito"
-              src={ favoriteIcon }
-              data-testid={ `${i}-horizontal-favorite-btn` }
-            />
-          </button>
-          <p data-testid={ `${i}-horizontal-top-text` }>
-            {recipe.type === 'drink'
-              ? recipe.alcoholicOrNot
-              : `${recipe.nationality} - ${recipe.category}`}
-          </p>
-        </div>
-      ))}
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ () => setFilter('') }
+      >
+        All
+        {' '}
+
+      </button>
+
+      {favoriteRecipes.length !== 0
+      && favoriteRecipes.filter((e) => (!filter ? e : e.type === filter))
+        .map((recipe, i) => (
+          <div id="mainAoQuadraxion" key={ recipe.id }>
+            <Link to={ `/${recipe.type}s/${recipe.id}` }>
+              <h2 data-testid={ `${i}-horizontal-name` }>{recipe.name}</h2>
+              <img
+                data-testid={ `${i}-horizontal-image` }
+                src={ recipe.image }
+                alt={ recipe.name }
+              />
+            </Link>
+            <button
+              type="button"
+              data-testid={ `${i}-sharebutton` }
+              onClick={ ({ target }) => {
+                target.innerHTML = 'Link copied!';
+                copy(`http://localhost:3000/${recipe.type}s/${recipe.id}`);
+              } }
+            >
+              <img
+                alt="Botão de Compartilhar"
+                src={ shareIcon }
+                data-testid={ `${i}-horizontal-share-btn` }
+              />
+            </button>
+
+            <button
+              type="button"
+              onClick={ () => removeFavoriteHandle(i) }
+            >
+              <img
+                alt="Botão de Favorito"
+                src={ favoriteIcon }
+                data-testid={ `${i}-horizontal-favorite-btn` }
+              />
+            </button>
+            <p data-testid={ `${i}-horizontal-top-text` }>
+              {recipe.type === 'drink'
+                ? recipe.alcoholicOrNot
+                : `${recipe.nationality} - ${recipe.category}`}
+            </p>
+          </div>
+        ))}
     </div>
   );
 }
